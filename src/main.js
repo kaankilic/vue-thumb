9832b05f9865608d626a0ca474453f59947fb0aa
Kaan Kilic <bl4cksta@gmail.com>
2019-05-28 17:41:08 +0300

    installer main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import component from './components/VueThumb.vue';
 
-Vue.config.productionTip = false
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+	if (install.installed) return;
+	install.installed = true;
+	Vue.component('VueThumb', component);
+}
 
-new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+// Create module definition for Vue.use()
+const plugin = {
+	install,
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+	GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+	GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+	GlobalVue.use(plugin);
+}
